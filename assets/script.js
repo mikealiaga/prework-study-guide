8ec421822701b77595f266300a217d114ed75182
mike aliaga <mikealiaga@192.168.1.19>
2024-09-08 20:12:22 -0600

    README file

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,17 @@
+// Declare objects as array
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+// Create a function that assigns a random number
 const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
+// Add a +1 and print object list
 function listTopics() {
     for (let x = 0; x < topics.length; x++) {
       console.log(topics[x]);
     }
   }
 
+//Function that prints a random subject to study
 function selectTopic() {
     if (randomTopic === 'HTML') {
       console.log("Let's study HTML!");
@@ -20,7 +25,8 @@ function selectTopic() {
       console.log('Please try again!');
     }
   }
-  
+
+// Prints
 console.log('Which topic should we study first?');
 console.log('Here are the topics we learned through Prework:');
 listTopics()
